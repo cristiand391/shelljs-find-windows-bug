a122486164dacdf60b8f0f8fcd9ddf499f1965ad
Cristian Dominguez <cdominguez@salesforce.com>
2025-07-01 15:44:07 -0300

    chore: update assertions

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
 const shelljs = require('shelljs')
 const path = require('node:path')
 const assert = require('node:assert/strict');
-const os = require('node:os');
 
 const dirsFound = shelljs.find(`${process.cwd()}${path.sep}project`).stdout
 const expectedDirs = [
-    `${process.cwd()}${path.sep}project${os.EOL}`,
-    `${process.cwd()}${path.sep}project${path.sep}source${os.EOL}`,
-    `${process.cwd()}${path.sep}project${path.sep}source${path.sep}index.js${os.EOL}`,
+    `${process.cwd()}/project\n`,
+    `${process.cwd()}/project/source\n`,
+    `${process.cwd()}/project/source/index.js\n`,
 ]
 
 assert.equal(dirsFound, expectedDirs.join(""))
